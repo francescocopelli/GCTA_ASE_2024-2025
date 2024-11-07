15b65d5d9481cf7a34974c0840e16a6605dd910a
Francesco Copelli <francescocopelli28@gmail.com>
2024-11-07 17:30:36 +0100

    User admin auth folder

---
+++ user_admin_auth/app.py
@@ -0,0 +1,11 @@
+# create an hello world endpoint
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
