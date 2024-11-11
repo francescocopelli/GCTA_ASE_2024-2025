6b542d400190df76646b201dff40889c70201f3d
Francesco Copelli <francescocopelli28@gmail.com>
2024-11-11 17:39:35 +0100

    script for exporting db from the volume

---
+++ export.sh
@@ -0,0 +1,3 @@
+# make a bash script that take the database into the docker volume and export it in /dbdata/user_db/user.db
+
+docker cp gcta_ase_2024-2025-db-manager-1:/app ./dbdata/user_db/user.db
