d00b5786a83dc2df360a39c6c9664ebacc54da11
contisimone <contisimone4@gmail.com>
2024-11-14 11:37:43 +0100

    removed unused script

--- export.sh
+++
@@ -1,3 +0,0 @@
-# make a bash script that take the database into the docker volume and export it in /dbdata/user_db/user.db
-
-docker cp gcta_ase_2024-2025-db-manager-1:/app/user.db ./dbdata/users_db/user.db
