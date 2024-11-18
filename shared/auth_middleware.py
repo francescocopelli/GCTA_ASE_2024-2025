abe2b41e579252347cde52c4b7e3ab71eb074900
Francesco Copelli <francescocopelli28@gmail.com>
2024-11-18 14:48:22 +0100

    Added common utils folder
    - Added decorators
    - Added common requirements
    - Added script for running pip install

---
+++ shared/auth_middleware.py
@@ -0,0 +1,63 @@
+import logging
+from functools import wraps
+import jwt
+import requests
+from flask import request, abort
+from flask import current_app
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        logging.info("Token required checking")
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+        if not token:
+            logging.error("Token is missing")
+            abort(401, "Authentication Token is missing!")
+        try:
+            data=jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+            
+            rst = requests.get(f"http://player:5000/get_user/PLAYER/{data['user_id']}")
+            current_user = rst.json()
+            
+            if current_user is None:
+                abort(401, "Invalid Authentication token!")
+                
+            # if not current_user["active"]:
+            #     abort(403)
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+def admin_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        logging.info("Admin required check")
+        token = None
+
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+        if not token:
+            logging.error("Token is missing")
+            abort(401, "Authentication Token is missing!")
+        try:
+            data=jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+
+            rst = requests.get(f"http://player:5000/get_user/ADMIN/{data['user_id']}")
+            current_user = rst.json()
+
+            if current_user is None:
+                abort(401, "Invalid Authentication token!")
+
+            if not current_user["admin"]:
+                abort(403)
+
+    return decorated
