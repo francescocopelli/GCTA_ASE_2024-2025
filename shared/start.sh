303297158628825c8223600e3b20ba05778b2bbc
Francesco Copelli <francescocopelli28@gmail.com>
2024-11-19 15:28:16 +0100

    Update start.sh
    Fixed

--- shared/start.sh
+++ shared/start.sh
@@ -1,7 +1,3 @@
-# first thing that has to be performed is pip install -r /app/shared/requirements.txt
-# then run flask on port 5000 and host 0.0.0.0
-
-#!/bin/bash
-
-pip install -r /app/shared/requirements.txt
+#!/bin/sh
+pip install -r "/app/shared/requirements.txt"
 flask run --host=0.0.0.0 --port=5000
